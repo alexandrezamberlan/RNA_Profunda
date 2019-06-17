fe93d43d5fde6bd0757a506a72902be6b87f66d5
Alexandre de O. Zamberlan <alexandre.zamberlan@unifra.edu.br>
2019-06-16 23:27:44 -0300

    Update vetorizacao2.py

--- vetorizacao2.py
+++ vetorizacao2.py
@@ -3,9 +3,18 @@
 #v = [v1, ..., vn] -> u = [exp(v1), exp(v2), ..., exp(vn)]
 
 #solução não vetorizada seria
+#Numpy é um pacote para computação científica. Mantida pela comunidade (www.numpy.org). 
 import numpy as np
 import time
 
+n = 1000000
+v = np.random.rand(n)
+
 u = np.zeros((n,1))
+
 for i in range(n):
   u[i] = math.exp(v[i])
+
+  
+#solução vetorizada
+u = np.exp(v)
