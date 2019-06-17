186c20b65a1999a920d220c774633f70901bc286
Alexandre de O. Zamberlan <alexandre.zamberlan@unifra.edu.br>
2019-06-17 00:07:00 -0300

    Create 5_reshape.py

---
+++ 5_reshape.py
@@ -0,0 +1,11 @@
+# duas funções importantes no pacote numpy para deep learning são np.shape() e np.reshape()
+# x.shape é utilizada para descobrir o shape (dimensão) da matriz/vetor x
+# x.reshape(...) é usado para remodelar x em outra dimensão
+
+'''
+Na Ciência da Computação, por exemplo, uma imagem é uma matriz 3D de shape (length,height,depth=3)(length,height,depth=3). 
+Porém, quando uma imagem é lida como entrada de um algoritmo, é necessário converter a imagem para um vetor de 
+shape (length∗height∗3,1)(length∗height∗3,1)
+
+Essa expressão é 'desenrolar' ou redesenhar/remodelar (reshape) a matriz 3D em um vetor 1D.
+'''
