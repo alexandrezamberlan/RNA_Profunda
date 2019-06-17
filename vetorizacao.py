74719ca3d37c93c137c76ccdd170b6c7e48d0466
Alexandre de O. Zamberlan <alexandre.zamberlan@unifra.edu.br>
2019-06-16 22:39:22 -0300

    Update vetorizacao.py

--- vetorizacao.py
+++ vetorizacao.py
@@ -1,3 +1,7 @@
+#em redes neurais profundas, laços dentro de laços, com a ideia de Big Data, pode aumentar a complexidade
+#pois, há multiplicações de matrizes/vetores..... 
+#dessa forma, python fornece o conceito de vetorização via numpy e o método dot()
+
 import numpy as np
 import time
 
@@ -11,4 +15,18 @@ inicio = time.time()
 c = np.dot(a,b)
 fim = time.time()
 
+print(c)
 print("Soluçao vetorizada: " + str(1000*(fim - inicio)) + " ms")
+
+
+#solução não vetorizada, ou seja, uso clássico de for
+c = 0
+inicio = time.time()
+for i in range(1000000):
+  c += a[i] * b[i]
+fim = time.time()
+
+print(c)
+print("Soluçao com for: " + str(1000*(fim - inicio)) + " ms")
+  
+#ou seja, a solução vetorizada é (aproximado) 300 vezes mais rápida neste exemplo  
