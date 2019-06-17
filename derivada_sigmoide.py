3fb890f1329146ecf4a102564b7af715873162a5
Alexandre de O. Zamberlan <alexandre.zamberlan@unifra.edu.br>
2019-06-16 23:53:57 -0300

    Update derivada_sigmoide.py

--- derivada_sigmoide.py
+++ derivada_sigmoide.py
@@ -1,4 +1,9 @@
 import numpy as np
 
-def sigmide_derivada(x):
+def sigmoiide_derivada(x):
   s = 1 / (1 + np.exp(-x))
+  ds = s * (1 - s)
+  return ds
+
+x = np.array([1, 2, 3])
+print ("A derivada sigmóide de x = " + str(sigmoiide_derivada(x)))
