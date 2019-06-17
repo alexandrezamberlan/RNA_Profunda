d3d8e3cbe49d6b0abb1c0da2f86ed15c2eb49287
Alexandre de O. Zamberlan <alexandre.zamberlan@unifra.edu.br>
2019-06-16 22:49:44 -0300

    Create vetorizacao2.py

---
+++ vetorizacao2.py
@@ -0,0 +1,6 @@
+#imagine que seja preciso aplicar o exponencial para cada elemento de uma matriz/vetor
+
+#v = [v1, ..., vn] -> u = [exp(v1), exp(v2), ..., exp(vn)]
+
+#solução não vetorizada seria
+import numpy as np
