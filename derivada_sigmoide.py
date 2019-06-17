a67ba25b3ecfd6122ffc4e5c0d7c7c315cc96d23
Alexandre de O. Zamberlan <alexandre.zamberlan@unifra.edu.br>
2019-06-16 23:58:09 -0300

    Update derivada_sigmoide.py

--- derivada_sigmoide.py
+++ derivada_sigmoide.py
@@ -1,4 +1,5 @@
-#a função de custo de uma RNA profunda é baseada no Gradiente Descendente, que por sua vez se utiliza da derivada da sigmoide....
+# a função de custo (média de todas as perdas) de uma RNA profunda é baseada no Gradiente Descendente, que por sua vez se utiliza da derivada da sigmoide....
+# os gradientes são usados para otimizar as funções de perda usando a retropropagação
 
 import numpy as np
 
