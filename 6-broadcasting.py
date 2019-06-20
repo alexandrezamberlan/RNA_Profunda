3234275fa623cb30ba42daf0f8be76fbbf52a93f
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2019-06-19 23:31:32 -0300

    Update 6-broadcasting.py

--- 6-broadcasting.py
+++ 6-broadcasting.py
@@ -1,4 +1,9 @@
 '''
+
+Um conceito muito importante para entender no pacote numpy é "broadcasting". 
+É muito útil para executar operações matemáticas entre arrays de diferentes dimensões.
+Veja http://docs.scipy.org/doc/numpy/user/basics.broadcasting.html
+
                 Maças   Carnes   Ovos   Batatas
 carboidratos    56      0.0      4.4    68   
 proteinas       1.2     104      52     8
@@ -25,3 +30,30 @@ Ideia geral de broadcasting é
 
 matriz(m,n)   (+ ou - ou * ou /)   matriz(1,n)  -> matriz(m,n)
 '''
+
+print("Outro exemplo de broadcasting.....")
+def softmax(x):
+    """Calcula softmax para cada linha de x.
+
+    Funciona em vetor e matrizes[n, m].
+
+    Argument:
+    x -- matriz do pacote numpy de dimensão (n,m)
+
+    Returns:
+    resultado -- matriz numpy igual ao softmax de x (n,m)
+    """
+    
+    x_elevada = np.exp(x)  
+
+    x_somada = np.sum(x_elevada, axis = 1, keepdims = True)
+    
+    resultado = x_elevada / x_somada
+    
+    return resultado
+  
+matriz = np.array([
+    [9, 2, 5, 0, 0],
+    [7, 5, 0, 0 ,0]])
+
+print("softmax da matriz = " + str(softmax(matriz)))  
