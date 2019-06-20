a76d8cf2522ff4b4755b1ab02e9b81078f9e9fa9
Alexandre de O. Zamberlan <alexandre.o.zamberlan@gmail.com>
2019-06-20 00:08:56 -0300

    Update 10-calcularPerdas.py

--- 10-calcularPerdas.py
+++ 10-calcularPerdas.py
@@ -10,6 +10,11 @@ def L1(yhat, y):
     perda = np.sum(abs(yhat - y))
     
     return perda
+
+
+def L2(yhat, y):
+    return np.sum(np.dot(y - yhat, y - yhat))    
+    
     
 #expectativa da saída em y
 yhat = np.array([.9, 0.2, 0.1, .4, .9])
@@ -17,5 +22,9 @@ yhat = np.array([.9, 0.2, 0.1, .4, .9])
 #realidade de saída em y
 y = np.array([1, 0, 0, 1, 1])
 
-#cálculo da perda
+#cálculo da perda L1
 print("L1 = " + str(L1(yhat,y)))    
+
+
+#cálculo da perda L2
+print("L1 = " + str(L2(yhat,y)))  
